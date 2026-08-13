88fd0c6682833773e44f301cadfb012482e5758c
freddy roland <freddyrolandhinkamma877@gmail.com>
2026-08-13 02:58:42 +0200

    configuration de environnement de travail

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,11 @@
+// // src/environments/environment.ts (dev)
+// export const environment = {
+//   production: false,
+//   apiUrl: 'http://127.0.0.1:8000/api'
+// };
+
+// src/environments/environment.prod.ts (prod)
 export const environment = {
-  production: false,
-  apiUrl: 'http://127.0.0.1:8000/api'
+  production: true,
+  apiUrl: 'https://bankconnectapi-production.up.railway.app/api'
 };
